080059ad771bc1b38a07f33cc5a8543eb3223b53
TheKicher <50838876+TheKicher@users.noreply.github.com>
2020-11-28 00:24:49 +0300

    -Создал новую активити для авторизации -В манифесте изменил стартовую активити -Сверстал авторизацию

--- rosseti-android/app/src/main/java/ru/bruh/rosseti/AuthorizationFragment.java
+++ rosseti-android/app/src/main/java/ru/bruh/rosseti/AuthorizationFragment.java
@@ -6,12 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class AuthorizationFragment extends MainActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_authorization);
+
     }
 
-}
+    public void click(View view) {
+        Intent intent = new Intent(AuthorizationFragment.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    }
