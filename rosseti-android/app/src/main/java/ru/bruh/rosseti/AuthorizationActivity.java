4e88482d7b67b4badb7d3d5c7335015884d1f54f
TheKicher <50838876+TheKicher@users.noreply.github.com>
2020-11-28 05:22:01 +0300

    -Исправление и доработка авторизации

--- rosseti-android/app/src/main/java/ru/bruh/rosseti/AuthorizationActivity.java
+++ rosseti-android/app/src/main/java/ru/bruh/rosseti/AuthorizationActivity.java
@@ -5,21 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import ru.bruh.rosseti.ui.activity.MainActivity;
 
-public class AuthorizationActivity extends AppCompatActivity{
+public class AuthorizationActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_authorization);
 
+        final Button button = findViewById(R.id.btnAuth);
+
+        button.setOnClickListener (new View.OnClickListener(){
+            public void onClick (View v) {
+
+                Intent intent = new Intent(AuthorizationActivity.this, MainActivity.class);
+                startActivity(intent);
+                String applicationName  = getString(R.string.to_come_in);
+            }
+    });
+    }
     }
 
-    public void click(View view) {
-        Intent intent = new Intent(AuthorizationActivity.this, MainActivity.class);
-        startActivity(intent);
-    }
 
-    }
+
+
